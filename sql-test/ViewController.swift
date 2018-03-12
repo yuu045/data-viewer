a15e802f562ec5b4ec75533b2ce386158e62c2dd
冉晴 <qingr@uw.edu>
2018-03-12 13:05:43 -0700

    Initial Commit

---
+++ sql-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sql-test
+//
+//  Created by Qing Ran on 3/12/18.
+//  Copyright © 2018 Qing Ran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sql-testTests/sql_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  sql_testTests.swift
+//  sql-testTests
+//
+//  Created by Qing Ran on 3/12/18.
+//  Copyright © 2018 Qing Ran. All rights reserved.
+//
+
+import XCTest
+@testable import sql_test
+
+class sql_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
